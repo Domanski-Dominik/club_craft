9a43d3f24b411f5c4b415b173a4de72ea15f72c6
Domanski-Dominik <domanbuty@gmail.com>
2024-05-22 11:35:43 +0200

    Does this count part2?

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,23 +55,6 @@ export default function RootLayout({
 				<meta
 					name='HandheldFriendly'
 					content='true'></meta>
-				<link
-					rel='apple-touch-icon'
-					sizes='180x180'
-					href='icons/favicon180x180.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='32x32'
-					href='icons/favicon-32x32.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='16x16'
-					href='icons/favicon-16x16.png'
-				/>
 			</head>
 			<ThemeRegistry>
 				<body>
