0357a89a23320aedb6f383cb02a0996bfc216049
Dominik <domanbuty@gmail.com>
2024-09-19 14:38:45 +0200

    Stats

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -5,11 +5,26 @@ import { format } from "date-fns/format";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import Loading from "@/context/Loading";
-import { Box, FormControl, Typography } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  Typography,
+  Tab,
+  Tabs,
+  Select,
+  InputLabel,
+  MenuItem,
+  Collapse,
+  Divider,
+} from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { Participant } from "@/types/type";
-import { MobileDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import {
+  MobileDatePicker,
+  LocalizationProvider,
+  DatePicker,
+} from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import {
   startOfMonth,
@@ -19,14 +34,70 @@ import {
 } from "date-fns";
 import pl from "date-fns/locale/pl";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import SelectInput from "@mui/material/Select/SelectInput";
+import {
+  Stack2,
+  StyledTab,
+  StyledTabs,
+} from "@/components/styled/StyledComponents";
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+      style={{ width: "100%" }}>
+      {value === index && (
+        <Box
+          sx={{
+            width: "100%",
+            backgroundColor: "white",
+            borderRadius: 3,
+            pt: 1,
+          }}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 const Stats = () => {
+  const [info, setInfo] = useState({
+    page: 0,
+    month: new Date(),
+    sumCash: 0,
+    sumTransfer: 0,
+    sumAll: 0,
+    presenceOption: "month",
+    location: "all",
+    rangeStart: new Date(),
+    rangeEnd: new Date(),
+  });
+  const [value, setValue] = useState(0);
   const [paymentData, setPaymentData] = useState<any>([]);
-  const [month, setMonth] = useState<Date>(new Date());
-  const [sumCash, setSumCash] = useState<number>(0);
-  const [sumTransfer, setSumTransfer] = useState<number>(0);
-  const [sumAll, setSumAll] = useState<number>(0);
-  const [owner, setOwner] = useState(false);
+  const [attendance, setAttendance] = useState<any>({
+    data: [],
+    series: [],
+    xAxis: [],
+  });
   const { status, data: session } = useSession({
     required: true,
     onUnauthenticated() {
@@ -41,19 +112,20 @@ const Stats = () => {
         `/api/participant/all/${session?.user.role}/${session?.user.club}/${session?.user.id}`
       ).then((res) => res.json()),
   });
-  useEffect(() => {
-    if (session?.user.role !== "owner") {
-      setOwner(false);
-    } else {
-      setOwner(true);
-    }
-  }, [session]);
+  const locWithGroups = useQuery({
+    queryKey: ["locWithGroups"],
+    enabled: !!session,
+    queryFn: () => fetch(`/api/components/form/${session?.user.club}`),
+  });
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
   useEffect(() => {
     if (participants.data) {
       // Tworzymy obiekt, który będzie przechowywał sumy płatności dla każdego dnia miesiąca
       const dailyPayments: any = {};
-      const startDate = startOfMonth(new Date(month));
-      const endDate = endOfMonth(new Date(month));
+      const startDate = startOfMonth(new Date(info.month));
+      const endDate = endOfMonth(new Date(info.month));
       const allDays = eachDayOfInterval({ start: startDate, end: endDate });
       // Przechodzimy przez wszystkich uczestników
       participants.data.forEach((participant) => {
@@ -104,15 +176,19 @@ const Stats = () => {
         (sum, data) => sum + data.transfer,
         0
       );
-      setSumCash(sumCash);
-      setSumTransfer(sumTransfer);
-      setSumAll(sumCash + sumTransfer);
+      setInfo((prev) => ({
+        ...prev,
+        sumCash: sumCash,
+        sumTransfer: sumTransfer,
+        sumAll: sumCash + sumTransfer,
+      }));
       // Teraz zmienna chartData zawiera tablicę obiektów {x: (numery od 1 do 31), cash: suma cash, transfer: suma transfer}
       //console.log(chartData);
       setPaymentData(chartData);
     }
     //console.log(participants.data);
-  }, [participants.data, month]);
+  }, [participants.data, info.month]);
+  useEffect(() => {}, [info, locWithGroups.data, participants.data]);
 
   if (status === "loading" || participants.isLoading) return <Loading />;
   if (participants.isError)
@@ -121,16 +197,33 @@ const Stats = () => {
         {participants.error.message}
       </Typography>
     );
-  if (!owner)
+  if (session.user.role === "coach")
     return (
       <Typography color={"red"} variant="h3" align="center">
         Nie masz uprawnień do tej strony
       </Typography>
     );
-  if (paymentData.length > 0 && owner)
-    return (
-      <>
-        <Grid container width={"100%"} spacing={3}>
+  return (
+    <Box
+      sx={{
+        height: "calc(100% - 80px)",
+        position: "absolute",
+        top: 60,
+        width: "100%",
+        p: 1,
+      }}>
+      <StyledTabs
+        value={value}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+        variant="scrollable"
+        scrollButtons="auto">
+        <StyledTab label="Płatności" {...a11yProps(0)} />
+        <StyledTab label="Obecności" {...a11yProps(1)} />
+      </StyledTabs>
+
+      <CustomTabPanel value={value} index={0}>
+        <Grid container width={"100%"} spacing={1} padding={2}>
           <Grid xs={6}>
             <FormControl
               fullWidth
@@ -145,10 +238,13 @@ const Stats = () => {
                 adapterLocale={pl}>
                 <MobileDatePicker
                   label="Miesiąc płatności"
-                  value={month}
+                  value={info.month}
                   onChange={(newDate) => {
                     if (newDate) {
-                      setMonth(newDate);
+                      setInfo((prev) => ({
+                        ...prev,
+                        month: newDate,
+                      }));
                       //console.log(newDate);
                     }
                   }}
@@ -162,15 +258,25 @@ const Stats = () => {
           </Grid>
           <Grid xs={6}>
             <Typography align="center">
-              Gotówka: <span style={{ color: "darkviolet" }}>{sumCash}</span>{" "}
-              zł, Przelew:{" "}
-              <span style={{ color: "darkviolet" }}>{sumTransfer}</span> zł,
+              Gotówka:{" "}
+              <span style={{ color: "darkviolet" }}>{info.sumCash}</span> zł,
+              Przelew:{" "}
+              <span style={{ color: "darkviolet" }}>{info.sumTransfer}</span>{" "}
+              zł,
               <br />
-              Suma: <span style={{ color: "darkviolet" }}>{sumAll}</span> zł
+              Suma: <span style={{ color: "darkviolet" }}>
+                {info.sumAll}
+              </span>{" "}
+              zł
             </Typography>
           </Grid>
         </Grid>
-        <Box width={"100%"} minWidth={360}>
+        <Divider variant="middle" />
+        <Box
+          sx={{
+            pt: 1,
+            width: "100%",
+          }}>
           <BarChart
             xAxis={[
               {
@@ -193,8 +299,175 @@ const Stats = () => {
             height={400}
           />
         </Box>
-      </>
-    );
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Stack2>
+          <Box width="50%" px={0.5}>
+            <FormControl fullWidth>
+              <InputLabel>Lokalizacje</InputLabel>
+              <Select value={info.location}>
+                <MenuItem value="all">Wszystkie</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box width="50%" px={0.5}>
+            <FormControl fullWidth>
+              <InputLabel>Zakres</InputLabel>
+              <Select
+                value={info.presenceOption}
+                onChange={(e) =>
+                  setInfo((prev) => ({
+                    ...prev,
+                    presenceOption: e.target.value,
+                  }))
+                }>
+                <MenuItem value={"month"}>Miesiąc</MenuItem>
+                <MenuItem value={"range"}>Zakres</MenuItem>
+                <MenuItem value={"day"}>Dzień</MenuItem>
+                <MenuItem value={"group"}>Grupa</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Stack2>
+        <Divider variant="middle" />
+        <Collapse in={info.presenceOption === "month"}>
+          <Stack2>
+            <FormControl
+              fullWidth
+              sx={{
+                alignItems: "center",
+                alignContent: "center",
+                justifyContent: "center",
+                justifyItems: "center",
+                px: 0.5,
+              }}>
+              <LocalizationProvider
+                dateAdapter={AdapterDateFns}
+                adapterLocale={pl}>
+                <MobileDatePicker
+                  label="Miesiąc"
+                  value={info.month}
+                  onChange={(newDate) => {
+                    if (newDate) {
+                      setInfo((prev) => ({
+                        ...prev,
+                        month: newDate,
+                      }));
+                      //console.log(newDate);
+                    }
+                  }}
+                  views={["month", "year"]}
+                  sx={{ width: "50%" }}
+                />
+              </LocalizationProvider>
+            </FormControl>
+          </Stack2>
+        </Collapse>
+
+        <Collapse in={info.presenceOption === "range"}>
+          <Stack2>
+            <Box width="50%" px={0.5}>
+              <FormControl fullWidth>
+                <LocalizationProvider
+                  dateAdapter={AdapterDateFns}
+                  adapterLocale={pl}>
+                  <DatePicker
+                    label="Od"
+                    value={info.rangeStart}
+                    onChange={(newDate) => {
+                      if (newDate) {
+                        setInfo((prev) => ({
+                          ...prev,
+                          rangeStart: newDate,
+                        }));
+                        //console.log(newDate);
+                      }
+                    }}
+                  />
+                </LocalizationProvider>
+              </FormControl>
+            </Box>
+            <Box width="50%" px={0.5}>
+              <FormControl fullWidth>
+                <LocalizationProvider
+                  dateAdapter={AdapterDateFns}
+                  adapterLocale={pl}>
+                  <DatePicker
+                    label="Do"
+                    value={info.rangeEnd}
+                    onChange={(newDate) => {
+                      if (newDate) {
+                        setInfo((prev) => ({
+                          ...prev,
+                          rangeEnd: newDate,
+                        }));
+                        //console.log(newDate);
+                      }
+                    }}
+                  />
+                </LocalizationProvider>
+              </FormControl>
+            </Box>
+          </Stack2>
+        </Collapse>
+
+        <Collapse in={info.presenceOption === "day"}>
+          <Stack2>
+            <FormControl
+              fullWidth
+              sx={{
+                alignItems: "center",
+                alignContent: "center",
+                justifyContent: "center",
+                justifyItems: "center",
+                px: 0.5,
+              }}>
+              <LocalizationProvider
+                dateAdapter={AdapterDateFns}
+                adapterLocale={pl}>
+                <MobileDatePicker
+                  label="Dzień"
+                  value={info.month}
+                  onChange={(newDate) => {
+                    if (newDate) {
+                      setInfo((prev) => ({
+                        ...prev,
+                        month: newDate,
+                      }));
+                      //console.log(newDate);
+                    }
+                  }}
+                  sx={{ width: "50%" }}
+                />
+              </LocalizationProvider>
+            </FormControl>
+          </Stack2>
+        </Collapse>
+        <Collapse in={info.presenceOption === "group"}>
+          <Stack2
+            sx={{
+              alignItems: "center",
+              alignContent: "center",
+              justifyContent: "center",
+              justifyItems: "center",
+              width: "100%",
+            }}>
+            <FormControl fullWidth sx={{ width: "50%", px: 0.5 }}>
+              <InputLabel id="select-group">Grupa</InputLabel>
+              <Select id="select-group" label="Grupa"></Select>
+            </FormControl>
+          </Stack2>
+        </Collapse>
+        <Divider variant="middle" />
+        <BarChart
+          xAxis={attendance.xAxis}
+          dataset={attendance.data}
+          series={attendance.series}
+          height={350}
+        />
+      </CustomTabPanel>
+    </Box>
+  );
 };
 
 export default Stats;
