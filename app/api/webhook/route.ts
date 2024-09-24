7934aeaf4784adbb697f0d9982d24059801ec954
Domanski-Dominik <domanbuty@gmail.com>
2024-09-24 22:15:17 +0200

    First payment update fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,8 +3,6 @@ import { prisma } from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export const config = { api: { bodyParser: false } };
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 	apiVersion: "2024-06-20",
 });
